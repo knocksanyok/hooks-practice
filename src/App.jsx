80dcd014bb87fc84436c871da13ed62ce77ac3cb
knocksanyok <utkin.alexandr.vladimirovich@gmail.com>
2025-08-27 17:07:22 +0300

    Prepared for the next time practice, 41:32

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,9 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
-  const [todos, setTodos] = useState([])
 
     console.log("mount App component")
-    //
-    // useEffect(() => {
-    //     console.warn('useEffect called for fetching todos')
-    //     fetch('https://jsonplaceholder.typicode.com/todos')
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             // console.log(data)
-    //             setTodos(data)
-    //         })
-    // }, [count])
-    //
-    // useLayoutEffect(() => {
-    //     console.error('useLayoutEffect')
-    // }, []);
-    //
-    // useEffect(() => {
-    //     if(!(count % 2 === 0)) return
-    //     // console.log('useEffect', {count});
-    // }, []);
+
 
     useEffect(() => {
         console.warn('useEffect has been called')
@@ -49,16 +29,6 @@ function App() {
           }}>
           count is {count}
         </button>
-
-        <button
-          onClick={() => {
-            setCount2((prev) => prev + 1);
-          }}>
-          count2 is {count2}
-        </button>
-
-          {JSON.stringify(todos)}
-
       </div>
     </>
   );
