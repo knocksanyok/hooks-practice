9aafab514e0502949d051ceeada6f703007ad070
knocksanyok <utkin.alexandr.vladimirovich@gmail.com>
2025-08-29 20:28:50 +0300

    Added useRef, useID

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,54 @@
-import {useEffect, useMemo, useLayoutEffect, useState, useCallback} from "react";
+import {useEffect, useMemo, useLayoutEffect, useState, useCallback, useRef, use, useId} from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function difficultCountForMemo() {
-    let difficultCount = 0;
-    for (let i = 0; i < 10; i++) {
-        Math.sqrt(i)
-        difficultCount++;
-    }
-    return difficultCount;
-}
 
 function App() {
+
     console.log("mount App component")
     const [count, setCount] = useState(0);
+    const countRef = useRef(0)
+    const id = useId();
+    const id2 = useId();
+    console.log({id, id2})
 
-    let difficultCount = useMemo(() => {
-        return difficultCountForMemo();
-    },[])
+
 
     const handleClick = useCallback(() => {
         setCount((prev) => prev + 1);
     },[setCount]);
 
+
+    const handleClickRef = useCallback(() => {
+        countRef.current += 1;
+    },[setCount]);
+
+
     useEffect(() => {
-        console.log('MathPI,' + ' ' + difficultCount);
-    },[difficultCount]);
+        if(!countRef.current)
+            return;
+        console.log(countRef.current);
+    },[countRef]);
 
 
 
   return (
     <>
       <div className="card">
+
         <button
           onClick={handleClick}>
           count is {count}
         </button>
+
+          <button
+              onClick={handleClickRef}>
+              countRef is {countRef.current}
+          </button>
+
+
+
       </div>
     </>
   );
